d85109b442514282294b17148d353bc517114811
makoma mabilo <makoma44@gmail.com>
2023-12-11 22:22:35 +0800

    fetching meals

--- biteswiftapp/src/assets/components/Meals.jsx
+++ biteswiftapp/src/assets/components/Meals.jsx
@@ -1,7 +1,46 @@
-
+import {useEffect, useState} from 'react';
 
  const Meals = () => {
+ const [meals, setMeals] = useState([]);
+ const [loading, setLoading] = useState(true);
 
+ useEffect(() => {
+
+  const fetchMeals = async () => {
+
+     try {
+    const response = await fetch('http://localhost:3000/meals');
+    const meals = await response.json();
+
+    setMeals(meals)
+    setLoading(false)
+
+    } catch (error) {
+      console.log('Error fetching meals:', error);
+      setLoading(false)
+
+    }
+
+  };
+
+    fetchMeals();
+
+ }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2>Meal List</h2>
+      <ul>
+        {meals.map(meal => (
+          <li key={meal.id}>{meal.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Meals;
