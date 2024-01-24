fdbee36e4df3809909c9d9025f84dec3f97b1d42
makoma mabilo <makoma44@gmail.com>
2024-01-24 22:06:59 +0800

    added UserProgressContext

---
+++ biteswiftapp/src/store/UserProgressContext.jsx
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+
+const UserProgressContext = createContext({
+progress: '', //" cart", "checkout"
+showCart: () => {},
+hideCart: () => {},
+showCheckout: () => {},
+hideCheckout: () => {}
+
+});
+
+export function UserProgressContextProvider({children}) {
+
+return (
+  <UserProgressContext.Provider>{children}</UserProgressContext.Provider>
+);
+
+}
+
+export default UserProgressContext;
